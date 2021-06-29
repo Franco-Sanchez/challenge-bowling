054a190df939f3ee2f433ec179433ff3e0f05731
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-06-29 12:14:08 -0500

    Read a file across the command line

--- start.rb
+++ start.rb
@@ -1,8 +1,15 @@
+text_file = ARGV.first
+
+p text_file
+
 player = {}
 
-File.foreach('./attempts.txt') do |line|
+File.foreach(text_file) do |line|
   name, point = line.split(' ').map { |element| /\A\d+\Z/.match?(element) ? element.to_i : element }
   point = 0 if point == 'F'
+  next if point.negative? || point > 10
 
-  
+  p name
+  p point
+
 end
